09658b32a87f2f273b869f50061e6751f61194a4
LetoSipra(Yusuf) <yusufakcay22.0@gmail.com>
2023-03-17 23:54:29 +0300

    update

---
+++ pages/cart.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+function Cart() {
+  return (
+    <>
+      <Head>
+        <title>Bag</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main>
+        <div>
+            <h1>
+                  AYAYAY
+            </h1>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default Cart;
